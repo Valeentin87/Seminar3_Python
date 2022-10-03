f8fcab05c131f206197719efb4fe9362d632bfb4
Valeentin87 <Valeentin87@list.ru>
2022-10-03 17:35:03 +0300

    успешное решение задачи 3

--- Task3.py
+++ Task3.py
@@ -1,2 +1,20 @@
 # Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между максимальным \
 # и минимальным значением дробной части элементов.
+import random
+N = int(input('введите число элементов в списке N = '))
+lst = [None]*N
+for i in range(N):
+    lst[i] = round(random.uniform(0.01, 20), 2)
+print(lst)
+max_fractional = lst[1]*100%100/100
+min_fractional = lst[i]*100%100/100
+for i in range(len(lst)):
+    if lst[i]*100%100/100>max_fractional:
+        max_fractional = lst[i]*100%100/100
+    if lst[i]*100%100/100<min_fractional:
+        min_fractional = lst[i]*100%100/100
+answer = max_fractional - min_fractional
+print(f'разница между максимальной и минимальной дробной частью в последовательности равна {answer}')
+
+
+
