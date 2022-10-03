a76774815406dfa57058df1a4daff910e6467c95
Valeentin87 <Valeentin87@list.ru>
2022-10-03 19:23:32 +0300

    успешное решение задачи 5

--- Task5.py
+++ Task5.py
@@ -1,1 +1,7 @@
 # Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
+N = int(input('введите число N = '))
+fib1 = fib2 = 1
+print(fib1, fib2, end=' ')
+for i in range(-N, N):
+    fib1, fib2 = fib2, fib1 + fib2
+    print(fib2, end=' ')
