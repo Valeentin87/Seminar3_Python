41ec2975d40319b7fad402a10bd034aef2d9eb06
Valeentin87 <Valeentin87@list.ru>
2022-10-03 16:31:41 +0300

    успешеное решение задачи 2

--- Task2.py
+++ Task2.py
@@ -6,3 +6,18 @@ lst = [None]*N
 for i in range(N):
     lst[i] = random.randint(0, 20)
 print(lst)
+multiplier = None
+if len(lst)%2 == 0:
+    multiplier = len(lst)/2
+else:
+    multiplier = len(lst)//2 + 1
+lst_result = [None]*int(multiplier)
+left_index = 0
+right_index = len(lst) - 1
+while left_index<=right_index:
+    lst_result[left_index] = lst[left_index]*lst[right_index]
+    left_index += 1
+    right_index = right_index - 1
+print(f' произведение сответствующих элементов списка {lst} равно {lst_result}')
+
+
