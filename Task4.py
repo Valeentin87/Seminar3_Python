e5b2f397634b424e770a43ebee461a458a9e3720
Valeentin87 <Valeentin87@list.ru>
2022-10-03 18:44:43 +0300

    успешное решение задачи 4

--- Task4.py
+++ Task4.py
@@ -2,9 +2,15 @@
 number = int(input('введите число в десятичной системе исчисления'))
 remains = 0
 whole = 0
-binary = ' '
-while number//2 !=0 and number//2 != 1:
-    remain = number%2
-    binary = binary + str(remain)
-    number = number//2
-print(binary)
+binary = ''
+while number // 2 != 0:
+    remains = number % 2
+    binary = binary + str(remains)
+    number = number // 2
+
+result_binary = str(number)
+j = 0
+for i in range(len(binary)):
+    result_binary = result_binary + binary[j-1]
+    j = j-1
+print(f'результат в двоичной системе исчисления равен {result_binary}')
